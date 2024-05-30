a9e8c76d9e5c04ab1c53ff8bc279576f6de000d6
Walace Ramos <contatowalace.dev@gmail.com>
2024-05-30 13:22:52 -0300

    update

--- Bootcamps Back End JAVA - Santander/Anatomia de classes - 1/java-anatomia-de-classes-01/src/MinhaClasse01.java
+++ Bootcamps Back End JAVA - Santander/Anatomia de classes - 1/java-anatomia-de-classes-01/src/MinhaClasse01.java
@@ -1,7 +1,12 @@
 public class MinhaClasse01 {
     public static void main (String [] args ) {
+
         String meuNome = "Walace Ramos";
+
         int anoFabricacao = 1992;
+        
         boolean vedadeira = true;
+
+        anoFabricacao = 1993;
     }
 }
