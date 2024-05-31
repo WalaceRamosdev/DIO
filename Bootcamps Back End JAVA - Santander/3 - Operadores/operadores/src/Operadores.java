5d8a3b6738a0e5b3f05526ac7803fbb6e9020d41
Walace Ramos <contatowalace.dev@gmail.com>
2024-05-31 09:45:25 -0300

    operadores

--- Bootcamps Back End JAVA - Santander/3 - Operadores/operadores/src/Operadores.java
+++ Bootcamps Back End JAVA - Santander/3 - Operadores/operadores/src/Operadores.java
@@ -1,6 +1,14 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
-        System.out.println(- numero);
+        
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeira" : "falso";
+
+        System.out.println(resultado);
+
     }
 }
