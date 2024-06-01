e803d7c194a9bdf5b89f5be108344bd91fb4bd0f
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-01 12:58:17 -0300

    while

---
+++ Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/DocesJoao.java
@@ -0,0 +1,24 @@
+
+
+public class DocesJoao {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+
+        while(mesada > 0) {
+
+            Double valorDoce = valorAleatorio();
+            
+            if (valorDoce > mesada)
+                valorDoce = mesada;
+
+            System.out.println("Doce do valor: " + valorDoce + "Adicionado no carrinho");
+            mesada = mesada - valorDoce;
+
+        }
+
+            private static double valorAleatorio() {
+                return ThreadLocalRandom.current().nextDouble(2, 8);
+            }    
+        
+    }
+}
