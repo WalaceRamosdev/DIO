0b4498cfaeb6688855de1735ac6744d5b1b06ff7
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-01 11:59:01 -0300

    repetições

---
+++ Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/Repeticoes.java
@@ -0,0 +1,7 @@
+public class Repeticoes {
+    public static void main(String[] args) throws Exception {
+        for (int carneiro = 1; carneiro <= 20; carneiro ++){
+        System.out.println("Contando carneirinhos" + carneiro);
+        }
+    }
+}
