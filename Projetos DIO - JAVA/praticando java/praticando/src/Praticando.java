1db97feb6f5772f8eb48d4ad0f15c0c9631cb1b0
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-01 10:45:11 -0300

    pratice

--- Projetos DIO - JAVA/praticando java/praticando/src/Praticando.java
+++ Projetos DIO - JAVA/praticando java/praticando/src/Praticando.java
@@ -1,5 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Praticando {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
         
     }
 }
