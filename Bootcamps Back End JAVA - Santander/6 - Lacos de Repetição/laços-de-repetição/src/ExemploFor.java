c95d71a0ce56ed456f8d5ccd16291cedf233c719
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-01 12:16:24 -0300

    break

--- Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/ExemploFor.java
+++ Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/ExemploFor.java
@@ -2,8 +2,13 @@ public class ExemploFor {
     public static void main(String[] args) {
         String alunos[] = {"Walace", "Palloma", "Gabriel"};
 
-        for (int x = 0; x < alunos.length; x++) {
-            System.out.println("O aluno na posição x = " + x + " é " + alunos[x]);
+        //for (int x = 0; x < alunos.length; x++) {
+          //  System.out.println("O aluno na posição x = " + x + " é " + alunos[x]);
+
+        for (String aluno : alunos){
+            System.out.print(" O nome do aluno é: " + aluno+",");
+        }
+
         }
     }
-}
+
