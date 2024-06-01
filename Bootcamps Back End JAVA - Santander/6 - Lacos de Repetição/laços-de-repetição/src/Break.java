4676b13f3f8d98e689a635a00d338ecf01687218
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-01 12:25:00 -0300

    for

--- Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/Break.java
+++ Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/Break.java
@@ -1,5 +1,10 @@
 public class Break {
     public static void main(String[] args) throws Exception {
+        for (int numero = 1; numero <= 5; numero ++) {
+            if (numero == 3)
+            break;
 
+            System.out.println (numero);
+        }
     }    
 }
