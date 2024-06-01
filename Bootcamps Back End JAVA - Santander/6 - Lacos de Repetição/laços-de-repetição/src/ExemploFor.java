7bd8e9f79a61ce9afc1d781b9d65cacfd6671082
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-01 12:11:55 -0300

    repetiçoes

---
+++ Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        String alunos[] = {"Walace", "Palloma", "Gabriel"};
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno na posição x = " + x + " é " + alunos[x]);
+        }
+    }
+}
--- Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/Repeticoes.java
+++ Bootcamps Back End JAVA - Santander/6 - Lacos de Repetição/laços-de-repetição/src/Repeticoes.java
@@ -1,7 +1,8 @@
 public class Repeticoes {
     public static void main(String[] args) throws Exception {
         for (int carneiro = 1; carneiro <= 20; carneiro ++){
-        System.out.println("Contando carneirinhos" + carneiro);
+            System.out.println("Contando carneirinhos" + carneiro);
         }
+        System.out.println("Gabriel Dormiu");
     }
 }
