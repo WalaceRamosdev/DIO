7350a06da649ff067f65e77dd4936fd706c1dc85
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-19 16:36:46 -0300

    poo

--- Bootcamps Back End JAVA - Santander/10 - POO/Impressora/Impressora/src/Equipamentos/multifuncional/EquipamentoMultiFuncional.java
+++ Bootcamps Back End JAVA - Santander/10 - POO/Impressora/Impressora/src/Equipamentos/multifuncional/EquipamentoMultiFuncional.java
@@ -1,5 +1,22 @@
 package Equipamentos.multifuncional;
 
-public class EquipamentoMultiFuncional {
+import Equipamentos.copiadora.Copiadora;
+import Equipamentos.digitalizadora.Digitalizadora;
+import Equipamentos.impressora.Impressora;
+
+public class EquipamentoMultiFuncional implements Copiadora, Digitalizadora, Impressora {
     
+    public void copiadora() {
+        System.out.println("Copiando via equipamento multifuncional");
+    }
+
+    public void digitalizadora() {
+        System.out.println("Digitalizando via equipamento multifuncional");
+    }
+
+    public void impressora() {
+        System.out.println("Imprimindo via equipamento multifuncional");
+        }
+
+
 }
--- Bootcamps Back End JAVA - Santander/10 - POO/Impressora/Impressora/src/estabelecimento/Fabrica.java
+++ Bootcamps Back End JAVA - Santander/10 - POO/Impressora/Impressora/src/estabelecimento/Fabrica.java
@@ -1,5 +1,14 @@
 package estabelecimento;
 
+import Equipamentos.impressora.DeskJet;
+import Equipamentos.impressora.Impressora;
+import Equipamentos.impressora.LaserJet;
+import Equipamentos.multifuncional.EquipamentoMultiFuncional;
+
 public class Fabrica {
-    
+    public static void main(String[] args) {
+        Impressora impressora = new EquipamentoMultiFuncional();
+
+        impressora.imprimir();
+    }
 }
