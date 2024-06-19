4fbcf621ff787ab04376a057d3fd9ec8bf6493e9
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-19 11:14:19 -0300

    poo

---
+++ Bootcamps Back End JAVA - Santander/10 - POO/java-orientado-objetos/src/Carro.java
@@ -0,0 +1,15 @@
+public class Carro {
+    public void ligar() {
+        System.err.println("Carro ligado");
+    }
+
+    public void confereCombustivel() {
+        System.out.println("Conferindo combustivel");
+    }
+
+    public void confereCambio() {
+        System.out.println("Conferindo cambio em P");
+    }
+
+
+}
