408ad1086866dccd0f9e67bb3596959cad5f1ded
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-19 17:03:13 -0300

    finalizado

--- Bootcamps Back End JAVA - Santander/10 - POO/Impressora/Impressora/src/estabelecimento/Fabrica.java
+++ Bootcamps Back End JAVA - Santander/10 - POO/Impressora/Impressora/src/estabelecimento/Fabrica.java
@@ -1,12 +1,21 @@
 package estabelecimento;
 
+import Equipamentos.copiadora.Copiadora;
+import Equipamentos.digitalizadora.Digitalizadora;
 import Equipamentos.impressora.Impressora;
 import Equipamentos.multifuncional.EquipamentosMultiFuncional;
 
 public class Fabrica {
     public static void main(String[] args) {
-        Impressora impressora = new EquipamentosMultiFuncional();
+
+        EquipamentosMultiFuncional em = new EquipamentosMultiFuncional();
+
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
 
         impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
     }
 }
